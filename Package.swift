6e946789c57baae6be5441eef1b3ac1734d18c23
zane <39070793+zaneenders@users.noreply.github.com>
2024-05-13 11:45:20 -0600

    support for MacOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "FPCore",
+    platforms: [
+        .macOS("14.0"), .iOS("16.0"), .watchOS("9.0"), .tvOS("16.0"),
+        .visionOS("1.0"),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
