614d4b5243ff5a2ecd5e7eca88e2ea0d6025b61e
zane <39070793+zaneenders@users.noreply.github.com>
2024-06-14 20:46:24 -0600

    DocC added

--- Package.swift
+++ Package.swift
@@ -15,6 +15,11 @@ let package = Package(
             name: "FPCore",
             targets: ["FPCore"])
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            from: "1.3.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
